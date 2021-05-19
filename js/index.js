6eb09f593f423ae84c66e976b40741c92fa1687a
sarrahbe <73913798+sarrahbe@users.noreply.github.com>
2021-05-18 23:31:30 -0400

    Homework 19

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("your file is loaded correctly");
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    /*loader.style.height="100px";
+    loader.style.borderRadius="50%";*/
+    loader.style.visibility="hidden";
+});
